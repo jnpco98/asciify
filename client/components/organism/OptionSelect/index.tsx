562591dadbba0e28814596b8caf794070135e559
ukiyo-roa <jnpco.98@gmail.com>
2020-05-02 00:20:23 +1000

    Added option select component

--- client/components/organism/OptionSelect/index.tsx
+++ client/components/organism/OptionSelect/index.tsx
@@ -4,26 +4,62 @@ import Section from '@layout/Section';
 import { OptionsType, ValueType, Theme } from 'react-select';
 import Select from '@components/atom/Select';
 import * as S from './style';
+import Button from '@components/atom/Button';
+import Switch from '@components/atom/Switch';
 
 export type SelectOption = { value: string; label: string };
 
 const generateOptions: OptionsType<SelectOption> = [
-  { value: 'Text', label: 'Text' },
-  { value: 'HTML', label: 'HTML' },
-  { value: 'Pixels', label: 'Pixels' }
+  { value: 'text', label: 'Text' },
+  { value: 'html', label: 'HTML' },
+  { value: 'pixels', label: 'Pixels' }
 ];
 
+const colorModeOptions: OptionsType<SelectOption> = [
+  { value: 'default', label: 'Default' },
+  { value: 'monochromatic', label: 'Monochromatic' },
+  { value: 'black', label: 'Black' }
+];
+
+export type AsciiTextSettings = {
+  charRamp?: string;
+  preserveAspectRatio?: boolean;
+  pixelCountHorizontal?: number;
+  pixelCountVertical?: number;
+}
+
+export type AsciiHtmlSettings = AsciiTextSettings & { 
+  gap: number, colorMode: string 
+};
+
+export type AsciiPixelSettings = AsciiHtmlSettings;
+
 function OptionSelect() {
-  const [option, selectOption] = useState<ValueType<SelectOption>>();
-
+  const [option, selectOption] = useState<{ value: string }>();
   function handleSelect(selected: ValueType<SelectOption>) {
-    selectOption(selected);
+    selectOption((selected as any).value || '');
   }
-
+  
   return(
     <Section>
       <S.Container>
-        <Select options={generateOptions} defaultValue={generateOptions[0]} onSelect={handleSelect} className="generate-select" classNamePrefix="generate-select" instanceId="generate-select"/>
+        <Select placeholder="Select ascii output type" options={generateOptions} onSelect={handleSelect} className="generate-select" classNamePrefix="generate-select" instanceId="generate-select"/>
+        <S.SettingsDivider/>
+        <S.SettingsContainer>
+          <S.AspectRatioSwitch />
+          <S.SettingsRow>
+            <S.SettingsInput bordered placeholder="Pixel Width"></S.SettingsInput>
+            <S.SettingsInput bordered placeholder="Pixel Height"></S.SettingsInput>
+            <S.SettingsInput bordered placeholder="Gap"></S.SettingsInput>
+          </S.SettingsRow>
+          <S.SettingsRow>
+            <Select placeholder="Select color mode" options={colorModeOptions} onSelect={handleSelect} className="color-mode" classNamePrefix="color-mode" instanceId="color-mode"/>
+          </S.SettingsRow>
+          <S.SettingsRow>
+            <S.SettingsCharacterRamp bordered placeholder={`Enter the characters you want to get included in the ascii art. From the darkest to the lightest ex: "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:," ^\`'. "`}></S.SettingsCharacterRamp>
+          </S.SettingsRow>
+        </S.SettingsContainer>
+        <Button>Generate</Button>
       </S.Container>
     </Section>
   );
--- client/components/organism/OptionSelect/style.ts
+++ client/components/organism/OptionSelect/style.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Input, { InputType } from "@components/atom/Input";
+import Switch from "@components/atom/Switch";
 
 export const Container = styled.div`
   display: flex;
@@ -10,4 +12,48 @@ export const Container = styled.div`
   z-index: 2;
   position: relative;
   width: 100%;
+`;
+
+export const SettingsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+`;
+
+
+type InputProps = {
+  bordered?: boolean;
+  placeholder?: string;
+}
+
+export const SettingsDivider = styled.div`
+  width: 100%;
+  height: 0.0625rem;
+  margin-top: 1.5rem;
+  background: ${({ theme }) => theme.colors.backgroundTertiary};
+`;
+
+export const SettingsInput = styled(Input)<InputProps>``;
+
+export const SettingsCharacterRamp = styled(Input).attrs({ inputType: InputType.Multi })<InputProps>`
+  height: 7rem;
+`;
+
+export const AspectRatioSwitch = styled(Switch).attrs({ name: 'ascii-options-aspect-ratio' })`
+  margin-bottom: 1rem;
+  margin-left: auto;
+`;
+
+export const SettingsRow = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: 0.5rem;
+
+  & > * {
+    width: 100%;
+
+    & + * {
+      margin-left: 0.5rem;
+    }
+  }
 `;
