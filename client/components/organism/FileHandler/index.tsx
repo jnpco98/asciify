6182ce2cb34963553022716e6998efeab0fe84d8
ukiyo-roa <jnpco.98@gmail.com>
2020-05-02 20:35:28 +1000

    Added custom proxy in the file upload

--- client/components/organism/FileHandler/index.tsx
+++ client/components/organism/FileHandler/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useRef } from 'react';
+import React, { FormEvent, useRef, useState } from 'react';
 import Section from '@layout/Section';
 import Text, { TextType } from '@components/atom/Text';
 import { FileDropFormat } from '../FileDrop';
@@ -11,6 +11,8 @@ type Props = {
 
 function FileHandler(props: Props) {
   const { onFileSelect } = props;
+  const [loading, setLoading] = useState(false);
+  
   const urlInputRef = useRef(null);
 
   async function handleOnUrlSubmit(e: FormEvent) {
@@ -19,15 +21,16 @@ function FileHandler(props: Props) {
     if(!urlInputValue || !urlInputValue.length) return;
 
     try {
-      // const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
-      const corsAnywhere = '';
-      const { data } = await axios({ method: 'get', url: corsAnywhere + urlInputValue, responseType: 'arraybuffer', proxy: {host: 'localhost', port: 5000} });
+      setLoading(true);
+      const corsAnywhere = 'https://ukiyo-cors-anywhere.herokuapp.com/';
+      const { data } = await axios({ method: 'get', url: corsAnywhere + urlInputValue, responseType: 'arraybuffer' });
       const imageBase64 = `data:;base64,${btoa(new Uint8Array(data).reduce((data, byte) => data + String.fromCharCode(byte), ''))}`;
       const imageData = { data: imageBase64, name: urlInputValue.split(/\//gi).pop(), size: new Buffer(imageBase64, 'base64').length }
       onFileSelect(imageData);
     } catch(e) {
-      console.error('error', e)
+      console.error(e);
     }
+    setLoading(false);
     if(urlInputRef.current) urlInputRef.current.value = '';
   }
 
