39a3651abf88072f49c4d6de644ff65c2ba94e15
ukiyo-roa <jnpco.98@gmail.com>
2020-05-03 20:16:34 +1000

    Added some basic type utilities. Added some build scripts

---
+++ client/utilities/types.ts
@@ -0,0 +1,11 @@
+export type KeysOfType<T, U> = { [k in keyof T]: T[k] extends U ? k : never }[keyof T];
+
+export interface ClassType<T = any> {
+  new (...args: any[]): T;
+}
+
+export declare type Maybe<T> = T | null | undefined;
+
+export declare type MaybePromise<T> = Promise<T> | T;
+
+export type Optional<T> = { [P in keyof T]? :T[P] }
