6e1639c394afbb42cad89b6212642ee3a4e53c9a
ukiyo-roa <jnpco.98@gmail.com>
2020-05-05 13:54:53 +1000

    Updated ascii html to use pre tags instead of divs. Added utility getters and setters.

--- services/generate/src/lib/ascii/modifiers/ascii-html.ts
+++ services/generate/src/lib/ascii/modifiers/ascii-html.ts
@@ -1,6 +1,5 @@
-import escapeHtml from 'escape-html';
 import { AsciiText } from './ascii-text';
-import { AsciiOutputModifierApplyParams, Color } from './ascii-output-modifier';
+import { AsciiOutputModifierApplyParams, Color, AsciiOutputModifierResult } from './ascii-output-modifier';
 
 export interface StyleSheetProperty {
   [key: string]: string | number;
@@ -22,9 +21,8 @@ export class AsciiHtml extends AsciiText {
   protected colorMode: ColorMode;
   protected gap: number;
 
-  public constructor(charRamp?: string[]) {
-    super(charRamp);
-
+  public constructor() {
+    super();
     // Initialize classes first
     this.containerClass = 'asc';
     this.rowClass = 'asc__row';
@@ -32,19 +30,24 @@ export class AsciiHtml extends AsciiText {
 
     // Use classes for stylesheets
     this.setStyleSheet({
+      [this.getContainerClass()]: {
+        background: '#343a40',
+        overflow: 'auto',
+        ['text-align']: 'center',
+        ['font-weight']: 'bold',
+        ['white-space']: 'pre',
+        ['box-sizing']: 'border-box',
+        ['font-size']: '0.8rem',
+        ['line-height']: 1.2,
+        ['font-family']: 'monospace'
+      },
       [this.getElementClass()]: {
-        width: '1rem',
-        height: '1rem',
         display: 'inline-block',
         ['box-sizing']: 'border-box',
-        ['font-family']: 'sans-serif',
-        ['text-align']: 'center',
-        ['margin-right']: '1px',
       },
       [this.getRowClass()]: {
         display: 'flex',
         ['box-sizing']: 'border-box',
-        ['margin-bottom']: '1px',
       },
     });
 
@@ -53,44 +56,43 @@ export class AsciiHtml extends AsciiText {
     this.setColorMode('default');
   }
 
-  protected transformColors(color: Color) {
-    let { r, g, b } = color;
-    if (this.getColorMode() === 'monochromatic') {
-      const monochrome = Math.ceil(0.21 * r + 0.72 * g + 0.07 * b);
+  protected transformColor(color: Color, colorMode: ColorMode): Color {
+    let { r, g, b, a } = color;
+    if (colorMode === 'monochromatic') {
+      const monochrome = Math.round(0.21 * r + 0.72 * g + 0.07 * b);
       (r = monochrome), (g = monochrome), (b = monochrome);
-    } else if (this.getColorMode() === 'black') {
+    } else if (colorMode === 'black') {
       (r = 0), (g = 0), (b = 0);
     }
-    return { r, g, b };
+    return { r, g, b, a };
   }
 
   // prettier-ignore
-  public apply(params: AsciiOutputModifierApplyParams) {
+  public apply(params: AsciiOutputModifierApplyParams, characterRamp: string): AsciiOutputModifierResult {
     const { data, colorData, info } = params;
     const colorMap: StyleSheet = {};
 
-    let html = `<div class="${this.getContainerClass()}"><div class="${this.getRowClass()}">`;
+    let html = `<pre class="${this.getContainerClass()}">`;
 
     for (let i = 0; i < data.length; i++) {
-      const color = data[i];
-      let { r, g, b } = this.transformColors({ ...colorData[i] });
+      let { r, g, b, a } = this.transformColor({ ...colorData[i] }, this.getColorMode());
       
-      const colorKey = `asc${r}${g}${b}`;
-      colorMap[colorKey] = { color: `rgb(${r}, ${g}, ${b})` };
+      const colorKey = `asc${r}${g}${b}${a * 255}`;
+      colorMap[colorKey] = { color: `rgba(${r}, ${g}, ${b}, ${a})` };
 
-      html += `<span class="${this.getElementClass()} ${colorKey}">${escapeHtml(this.getColorCharacter(color))}</span>`;
-      if((i + 1) % info.width === 0) html += `</div><div class="${this.getRowClass()}">`;
+      html += `<span class="${this.getElementClass()} ${colorKey}">@</span>`;
+      if((i + 1) % info.width === 0) html += '\n';
     }
 
-    html += `</div></div>`;
+    html += `</pre>`;
 
     return {
       style: this.createCssStyleSheet([this.getStyleSheet(), colorMap]),
-      ascii: html,
+      ascii: html
     };
   }
 
-  protected createCssStyleSheet(styleSheets: StyleSheet[]) {
+  protected createCssStyleSheet(styleSheets: StyleSheet[]): string {
     let accumulatedStyles = '';
     
     styleSheets.forEach(styleSheet => {
@@ -112,44 +114,61 @@ export class AsciiHtml extends AsciiText {
     return accumulatedStyles;
   }
 
-  public setStyleSheet(styleSheet: StyleSheet) {
+  public setStyleSheet(styleSheet: StyleSheet): void {
     this.styleSheet = styleSheet;
   }
-  public getStyleSheet() {
+
+  public getStyleSheet(): StyleSheet {
     return this.styleSheet;
   }
 
-  public getContainerClass() {
+  public getContainerClass(): string {
     return this.containerClass;
   }
 
-  public getRowClass() {
+  public getRowClass(): string {
     return this.rowClass;
   }
 
-  public getElementClass() {
+  public getElementClass(): string {
     return this.elementClass;
   }
 
-  public setColorMode(colorMode: ColorMode) {
+  public setColorMode(colorMode: ColorMode): void {
     if (colorMode === 'black' || colorMode === 'monochromatic')
       this.colorMode = colorMode;
     else this.colorMode = 'default';
   }
-  public getColorMode() {
+
+  public getColorMode(): ColorMode {
     return this.colorMode;
   }
 
-  public setGap(gap: number) {
+  public setGap(gap: number): void {
     this.styleSheet[this.getElementClass()]['margin-right'] = `${gap}px`;
     this.styleSheet[this.getRowClass()]['margin-bottom'] = `${gap}px`;
   }
 
-  public getGap() {
-    return this.styleSheet[this.getElementClass()]['margin-right'];
+  public getGap(): number {
+    let gap = this.styleSheet[this.getElementClass()]['margin-right'];
+    if(typeof gap === 'number') return gap;
+
+    if(gap && typeof gap === 'string') {
+      const pixel = gap.split('px').shift() || '';
+      return !isNaN(parseInt(pixel)) ? parseInt(pixel) : 0;
+    }
+    return 0;
   }
 
-  public modifierAllowsMinify() {
+  public modifierAllowsMinify(): boolean {
     return true;
+  }
+
+  public getStyle(element: string, property: string): string | number {
+    return this.styleSheet[element][property];
+  }
+
+  public setStyle(element: string, property: StyleSheetProperty): void {
+    this.styleSheet[element] = { ...this.styleSheet[element], ...property };
   }
 }
